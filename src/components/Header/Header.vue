828155a2d89323edf4337839fbd0994dcf3776f4
Paulo De Castro <zefiguefool@gmail.com>
2026-05-15 09:42:30 +0200

    Presentation du jeu raccourci et ajout d'un toggle pour les détails du jeu

--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -6,37 +6,60 @@
             <img v-else src="../../assets/images/sound-off.jpg" />
         </a>
         <div class="intro col-md-7 mx-auto">
-            <p class="intro-first-line">Voici un <a href="#anchor" class="anchor" aria-label="Go to Solver">Solver <span class="font-weight-bold p-0">&darr;</span></a> pour le jeu <a href="https://sutom.nocle.fr/" target="_blank" class="sutom-color">Sutom</a> qui fonctionne également pour son équivalent <a href="https://www.tusmo.xyz/" target="_blank" class="tuzmo-color">Tusmo</a> et pour la variante <a href="https://www.tusmo.xyz/" target="_blank" class="wordle-color">Wordle</a> (première lettre du mot à deviner aussi)</p>
-            <p>Entrez tel qu'affiché sur les trois jeux une<span class="text-uppercase">majuscule</span>pour une <span class="color-good-place tile">lettre</span> bien placée, une minuscule pour une <span class="bad-place tile">lettre</span> mal placée et un<span class="color-grid tile">tiret (-) </span> pour une lettre inconnue.<br />
-            Eliminez des lettres en activant <span class="color-grid tile">"Mode lettres à exclure"</span>.<br />
-            Lancez la recherche en cliquant sur <span class="search-button">Chercher</span></p>
-            <p>Les résultats de la recherche sont ensuite fournis en dessous&nbsp;<a href="#anchor" class="anchor">(*)</a> :</p>
+            <!-- <p class="intro-first-line">Voici un <a href="#anchor" class="anchor" aria-label="Go to Solver">Solver <span class="font-weight-bold p-0">&darr;</span></a> pour le jeu <a href="https://sutom.nocle.fr/" target="_blank" class="sutom-color">Sutom</a> qui fonctionne également pour son équivalent <a href="https://www.tusmo.xyz/" target="_blank" class="tuzmo-color">Tusmo</a> et pour la variante <a href="https://www.tusmo.xyz/" target="_blank" class="wordle-color">Wordle</a> (première lettre du mot à deviner aussi)</p>
+            
             <p><strong>Le solveur</strong> est affiché en premier. Les jeux Sutom, Tuzmo et Wordle sont affichés en dessous afin de vous permettre de jouer tout en vous aidant du solveur pour résoudre les grilles. Vous pouvez passer à la grille suivante à tout moment. Vous pouvez passer également d'un jeu à l'autre sans perdre votre progression.</p>
             <p class="mt-2"><a name="anchor"></a></p>
             <p>BONNE CHANCE ET AMUSEZ-VOUS BIEN !</p>
             <p class="font-smaller">
-            (*) : Le solver se base sur une liste de mots français courants, il est donc possible que certains mots soient manquants. N'hésitez pas à me signaler les mots manquants ou à me proposer des améliorations.</p>    
+            (*) : Le solver se base sur une liste de mots français courants, il est donc possible que certains mots soient manquants. N'hésitez pas à me signaler les mots manquants ou à me proposer des améliorations.</p>   -->
+            <p class="text-center">Comment utiliser le <a href="#anchor" class="anchor" aria-label="Go to Solver">Solver <span
+                        class="font-weight-bold p-0">&darr;</span></a> ?</p>
+            <ul>
+                <li><span class="color-good-place tile">A</span> = bien placée</li>
+                <li><span class="bad-place tile p-1">a</span> = présente mal placée</li>
+                <li><span class="color-grid tile">-</span> = inconnue</li>
+                <li><span class="color-grid tile">"Mode lettres à exclure"</span> = éliminer des lettres</li>
+                <li><span class="search-button">Chercher</span> = lancer la recherche</li>
+                <li><span class="select-game active">Sutom</span> - <span class="select-game">Tusmo</span> - <span class="select-game">Wordle</span> = Selectionner son jeu</li>
+            </ul>
+            <details open name="details-infos" class="details-infos">
+                <summary class="relative"><span>Plus d'informations sur le <a href="#anchor" class="anchor" aria-label="Go to Solver">solver</a></span></summary>
+                <div class="more-infos text-left">
+                    <p class="text-left pb-2">Fonctionne pour les jeux <a href="https://sutom.nocle.fr/" target="_blank" title="Aller sur le site de Sutom"
+                            class="sutom-color underline">Sutom<i class="fi fi-sc-up-right-from-square"></i></a>, <a
+                            href="https://www.tusmo.xyz/" target="_blank" title="Aller sur le site de Tusmo" class="tuzmo-color underline">Tusmo<i class="fi fi-sc-up-right-from-square"></i></a> et <a href="https://www.tusmo.xyz/" target="_blank" title="Aller sur le site de Wordle" class="wordle-color underline">Wordle<i class="fi fi-sc-up-right-from-square"></i></a>
+                    </p>
+                    <p><strong>Le solveur</strong> est affiché en premier. Les jeux Sutom, Tuzmo et Wordle sont affichés
+                        en dessous afin de vous permettre de jouer tout en vous aidant du solveur pour résoudre les
+                        grilles. Vous pouvez passer à la grille suivante à tout moment. Vous pouvez passer également
+                        d'un jeu à l'autre sans perdre votre progression.</p>
+                    <p>Le solver se base sur une liste de mots français courants, il est donc possible que certains mots
+                        soient manquants. N'hésitez pas à me signaler les mots manquants ou à me proposer des
+                        améliorations.</p>
+                </div>
+            </details>
         </div>
     </div>
 </template>
 <script>
-    import {EventBus} from '../../event-bus.js'
-    export default{
-        name: 'AppHeader',
-        data(){
-            return{
-                titreHTML: '<span class="color-good-place">S</span><span>o</span><span>l</span><span class="color-good-place">v</span><span>e</span><span>r</span><br><span class="color-bad-place">S</span><span>u</span><span class="color-bad-place">t</span><span>o</span><span>m</span>',
-                soundActive : false
-            }
-        },
-        methods: {
-            soundAction(){
-                this.soundActive = !this.soundActive;
-                EventBus.$emit('soundAction',this.soundActive); 
-            }
+import { EventBus } from '../../event-bus.js'
+export default {
+    name: 'AppHeader',
+    data() {
+        return {
+            titreHTML: '<span class="color-good-place">S</span><span>o</span><span>l</span><span class="color-good-place">v</span><span>e</span><span>r</span><br><span class="color-bad-place">S</span><span>u</span><span class="color-bad-place">t</span><span>o</span><span>m</span>',
+            soundActive: false
+        }
+    },
+    methods: {
+        soundAction() {
+            this.soundActive = !this.soundActive;
+            EventBus.$emit('soundAction', this.soundActive);
         }
     }
+}
 </script>
 <style scoped>
-    @import './Header.css';
+@import './Header.css';
 </style>
--- src/components/HeaderWordle/HeaderWordle.vue
+++ src/components/HeaderWordle/HeaderWordle.vue
@@ -6,9 +6,33 @@
             <img v-else src="../../assets/images/sound-off.jpg" />
         </a>
          <div class="intro col-md-7 mx-auto">
-            <p>Voici un Solver pour le jeu<a href="https://wordle.louan.me/" target="_blank">wordle</a> <img src="./../../assets/images/wordle-visuel.jpg" alt="visuel sotum" /></p>
+            <!-- <p>Voici un Solver pour le jeu<a href="https://wordle.louan.me/" target="_blank">wordle</a> <img src="./../../assets/images/wordle-visuel.jpg" alt="visuel sotum" /></p>
             <p>Entrez tel qu'affiché sur Wordle une majuscule pour une <span class="color-good-place">lettre</span> bien placée, une minuscule pour une <span class="color-bad-place">lettre</span> mal placée et un tiret (-) pour une <span class="color-grid text-white">lettre</span> inconnue. Eliminez des lettres en activant "Mode lettres à exclure". Enfin lancer la recherche en cliquant sur "Chercher".
-            </p>
+            </p> -->
+            <p class="intro-first-line">Comment utiliser le <a href="#anchor" class="anchor" aria-label="Go to Solver">Solver <span class="font-weight-bold p-0">&darr;</span></a> pour Wordle ?</p>
+            <ul>
+                <li><span class="color-good-place tile">A</span> ---- → lettre bien placée</li>
+                <li><span class="bad-place tile p-1">a</span> ---- → lettre présente mal placée</li>
+                <li><span class="color-grid tile">-</span> ---- → lettre inconnue</li>
+                <li><span class="color-grid tile">"Mode lettres à exclure"</span> ---- → éliminer des lettres</li>
+                <li><span class="search-button">Chercher</span> ---- → lancer la recherche</li>
+            </ul>
+            <details open name="details-infos" class="details-infos">
+                <summary class="relative"><span>Plus d'informations sur le <a href="#anchor" class="anchor" aria-label="Go to Solver">solver</a></span></summary>
+                <div class="more-infos text-left">
+                    <p class="text-left pb-2">Fonctionne pour les jeux <a href="https://sutom.nocle.fr/" target="_blank" title="Aller sur le site de Sutom"
+                            class="sutom-color underline">Sutom<i class="fi fi-sc-up-right-from-square"></i></a>, <a
+                            href="https://www.tusmo.xyz/" target="_blank" title="Aller sur le site de Tusmo" class="tuzmo-color underline">Tusmo<i class="fi fi-sc-up-right-from-square"></i></a> et <a href="https://www.tusmo.xyz/" target="_blank" title="Aller sur le site de Wordle" class="wordle-color underline">Wordle<i class="fi fi-sc-up-right-from-square"></i></a>
+                    </p>
+                    <p><strong>Le solveur</strong> est affiché en premier. Les jeux Sutom, Tuzmo et Wordle sont affichés
+                        en dessous afin de vous permettre de jouer tout en vous aidant du solveur pour résoudre les
+                        grilles. Vous pouvez passer à la grille suivante à tout moment. Vous pouvez passer également
+                        d'un jeu à l'autre sans perdre votre progression.</p>
+                    <p>Le solver se base sur une liste de mots français courants, il est donc possible que certains mots
+                        soient manquants. N'hésitez pas à me signaler les mots manquants ou à me proposer des
+                        améliorations.</p>
+                </div>
+            </details>
         </div>
     </div>
 </template>
